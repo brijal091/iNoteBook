17fb332dffe211c2818d7b596120dce3eab2df6a
Brijal <brijalkansara091@gmail.com>
2022-09-28 15:19:39 +0530

    Video 44 Completed

--- Backend/index.js
+++ Backend/index.js
@@ -1,3 +1,14 @@
 const connectToMongo = require('./db');
-
+const express = require('express')
 connectToMongo();
+
+const app = express()
+const port = 3000
+
+// Avaulable Routes
+app.use('/api/auth', require('./routes/auth'))
+app.use('/api/notes', require('./routes/notes'))
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
