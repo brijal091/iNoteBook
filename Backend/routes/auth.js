5eb55edf516352e846bc5435e081151dedbc80d3
Brijal <brijalkansara091@gmail.com>
2022-10-05 14:40:47 +0530

    Video number 47. Thunderclient new req

--- Backend/routes/auth.js
+++ Backend/routes/auth.js
@@ -3,19 +3,30 @@ const User = require('../models/User');
 const router = express.Router();
 const { body, validationResult } = require('express-validator');
 
-router.post('/', body('email').isEmail(), body('password',"Password must me more then 5 char").isLength({ min: 5 }),(req, res)=>{
-    const errors = validationResult(req);
+router.post('/createuser', body('email').isEmail(), body('password',"Password must me more then 5 char").isLength({ min: 5 }), async (req, res)=>{
+  // If errors occur return Bad req 
+  const errors = validationResult(req);
     if (!errors.isEmpty()) {
       return res.status(400).json({ errors: errors.array() });
     }
-    User.create({
+    // check weather the user is already in database 
+    try{
+    let user = await User.findOne({email: req.body.email})
+    if (user){
+      return res.status(400).json({error: "User with this email already exist."})
+    } 
+    user = await User.create({
       name: req.body.name,
       email: req.body.email,
       password: req.body.password,
-    }).then(user => res.json(user))
-    .catch(err => {console.log(err)})
-    res.json({error: "Please Enter a Unique Value"})
-  },
- )
+    })
+    res.json(user);
+  }
+    catch(error){
+      console.error(error.message);
+      res.status(500).send("Something went Wrong")
+    }
+    // res.json({error: "Please Enter a Unique Value"})
+  } )
 
 module.exports = router
