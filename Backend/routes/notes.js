f501a5274f920e4bc7919f6bd5d9ee31746514d0
Brijal <brijalkansara091@gmail.com>
2022-10-10 13:46:46 +0530

    Video 52. Done

--- Backend/routes/notes.js
+++ Backend/routes/notes.js
@@ -1,9 +1,40 @@
 const express = require('express');
 const router = express.Router();
+const fetchuser = require('../middleware/fetchuser');
+const Note = require('../models/Note');
+const { body, validationResult } = require('express-validator');
 
-router.get('/', (req, res)=>{
-
-    res.json([])
+router.get('/fetchallnotes',fetchuser,async (req, res)=>{
+    try {
+        const notes = await Note.find({user: req.user.id})
+        res.json(notes)
+        
+    } catch (error) {
+        console.error(error.message);
+        res.status(500).send("Something is wrong")
+    }
 } )
 
-module.exports = router
+router.post('/addnote',fetchuser,[
+    body('title', "Enter valid Title").isLength({min: 5}), 
+    body('discription',"Give atleast 10 char").isLength({ min: 10 })],async (req, res)=>{
+        try {
+            
+            const {title, description, tag} = req.body;
+            const error = validationResult(req);
+            if (!error.isEmpty){
+                return res.status(400).json({errors: error.array()});
+            }
+            
+            const note = new Note({
+                title, description, tag, user: req.user.id
+            })
+            const savedNote = await note.save();
+            res.json(savedNote)
+        } catch (error) {
+            console.error(error.message);
+            res.status(500).send("Something is wrong") 
+        }
+} )
+
+module.exports = router;
