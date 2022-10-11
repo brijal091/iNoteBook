6f7021d91956fa39b03035180c3e0b71ab2df5a7
Brijal <brijalkansara091@gmail.com>
2022-10-11 17:24:45 +0530

    Update and Delete end points of API Viedeo : 54 Completed

--- Backend/routes/notes.js
+++ Backend/routes/notes.js
@@ -37,4 +37,39 @@ router.post('/addnote',fetchuser,[
         }
 } )
 
+router.put('/updatenot/:id',fetchuser, async (req, res)=>{
+       const {title, description, tag}  = req.body 
+    //    Create a newNote object 
+    const newNote = {};
+    if (title){newNote.title = title};
+    if (description){newNote.description = description};
+    if (tag){newNote.tag = tag};
+
+    // Find the note to be updated 
+
+    // Readintg the id from the URL 
+    let note = await Note.findById(req.params.id)
+    if (!note){return res.status(404).send("Not Found")};
+    if (note.user.toString() !== req.user.id){
+        return res.status.send(401).send("Not allowed");
+    }
+// Here new: true means that if any new object is added to that note than it will be created 
+    note = await Note.findByIdAndUpdate(req.params.id, {$set: newNote}, {new: true})
+    res.json({note})
+} )
+
+
+router.delete('/deletenode/:id',fetchuser, async (req, res)=>{
+ // Readintg the id from the URL 
+ let note = await Note.findById(req.params.id)
+ if (!note){return res.status(404).send("Not Found")};
+
+ if (note.user.toString() !== req.user.id){
+     return res.status.send(401).send("Not allowed");
+ }
+// Here new: true means that if any new object is added to that note than it will be created 
+ note = await Note.findByIdAndDelete(req.params.id)
+ res.json({"sucess":"Note has been deleted"})
+} )
+
 module.exports = router;
